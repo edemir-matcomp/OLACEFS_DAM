016b319d47c7b8339c8ae7603714add232d8b77e
PedroFW <42937113+PedroFW@users.noreply.github.com>
2020-09-21 14:04:11 -0300

    Update utils.py

--- 4_Change_Detection/utils.py
+++ 4_Change_Detection/utils.py
@@ -30,3 +30,58 @@ def random_crop(img, mask, width, height):
     img = img[y:y+height, x:x+width]
     mask = mask[y:y+height, x:x+width]
     return img, mask
+
+# transform image into crops of specified size and stride
+
+def forward_crop(img, window, channels, stride):
+    crops = np.empty((((img.shape[0] - window[0])//stride + 1 + (img.shape[0] - window[0])%stride)*((img.shape[1] - window[1])//stride + 1 + (img.shape[1] - window[1])%stride), 32, 32, channels))
+    idx=0
+    stride_i = stride
+    stride_j = stride
+    ini_i = 0
+    ini_j = 0
+    for j in range((img.shape[1]- window[1])//stride + 1 +(img.shape[1] - window[1])%stride):
+        for i in range((img.shape[0]- window[0])//stride + 1 +(img.shape[0] - window[0])%stride):
+            crops[idx] = img[ini_i:window[0]+ini_i, ini_j:window[1]+ini_j].copy()
+            if ini_i + window[0] + stride > img.shape[0]:
+                stride_i = 1
+            else:
+                stride_i = stride
+            if ini_j + window[1] + stride > img.shape[1]:
+                stride_j = 1
+            else:
+                stride_j = stride
+            ini_i += stride_i
+            idx += 1
+        ini_i = 0
+        ini_j += stride_j
+    return crops
+
+# reconstruct the crops into the original image. 
+# any intersection between the crops (determined by the stride) is the mean between the pixels in it. 
+
+def reconstruct(crops, img_size, window, channels, stride):
+    img = np.zeros((img_size[0], img_size[1], channels))
+    sum_it = np.zeros((img_size[0], img_size[1], channels))
+    idx=0
+    stride_i = stride
+    stride_j = stride
+    ini_i = 0
+    ini_j = 0
+    for j in range((img.shape[1]- window[1])//stride + 1 + (img.shape[1] - window[1])%stride):
+        for i in range((img.shape[0]- window[0])//stride + 1 + (img.shape[0] - window[0])%stride):
+            img[ini_i:window[0]+ini_i, ini_j:window[1]+ini_j] += crops[idx]
+            sum_it[ini_i:window[0]+ini_i, ini_j:window[1]+ini_j] += 1
+            if ini_i + window[0] + stride > img.shape[0]:
+                stride_i = 1
+            else:
+                stride_i = stride
+            if ini_j + window[1] + stride > img.shape[1]:
+                stride_j = 1
+            else:
+                stride_j = stride
+            ini_i += stride_i
+            idx += 1 
+        ini_i = 0
+        ini_j += stride_j  
+    return img*(1/sum_it)
